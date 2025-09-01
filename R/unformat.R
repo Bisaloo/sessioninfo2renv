143ef461053ba6e7d1323970cf2cd18d00fb2ebe
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-09-01 17:30:13 +0200

    First draft for unformat

---
+++ R/unformat.R
@@ -0,0 +1,59 @@
+#̈́' @examples
+#' file <- system.file("extdata", "sessioninfo.txt", package = "sessioninfo")
+unformat_session_info <- function(file) {
+  x <- readLines(file)
+
+  platform_section <- grep("^─ Session info ─", x)
+  packages_section <- grep("^─ Packages ─", x)
+  footnotes <- grep("^$", x)
+
+  platform_header <- x[platform_section + 1]
+  colstarts <- regexec(" (setting)\\s+(value)", platform_header)[[1]]
+
+  # FIXME: I would expect we can get the header by skipping one line less and
+  # setting `header = TRUE` but it does not work.
+  platform <- read.fwf(
+    file = file,
+    widths = c(diff(colstarts), 50),
+    skip = platform_section + 1,
+    n = packages_section - platform_section - 3,
+  )
+  platform <- setNames(
+    as.list(platform[, 3]),
+    trimws(platform[, 2])
+  )
+  class(platform) <- c("platform_info")
+
+
+  packages_header <- x[packages_section + 1]
+  colstarts <- regexec(" (!) (package)\\s+(\\*)\\s+(version)\\s+(date \\(UTC\\))\\s+(lib)\\s+(source)", packages_header)[[1]]
+
+  packages <- read.fwf(
+    file = file,
+    widths = c(diff(colstarts), 50),
+    skip = packages_section + 1,
+    n = footnotes[2] - packages_section - 2
+  )
+  colnames(packages) <- c("skip", "warn", "package", "attached", "loadedversion", "date", "lib", "source")
+  packages <- as.data.frame(vapply(packages, trimws, character(nrow(packages))))
+  packages$attached <- packages$attached == "*"
+  packages <- packages[, colnames(packages) != "skip"]
+
+  # Guess
+  packages$library <- gsub("^\\[(\\d+)\\]\\s*", "\\1", packages$lib)
+  packages$ondiskversion <- packages$loadedversion
+    packages$md5ok <- NA
+  packages$isbase <- FALSE
+  packages$path <- "placeholder"
+  packages$loadedpath <- "placeholder"
+
+  class(packages) <- c("packages_info", "data.frame")
+
+  structure(
+    list(
+      platform = platform,
+      packages = packages
+    ),
+    class = "session_info"
+  )
+}
