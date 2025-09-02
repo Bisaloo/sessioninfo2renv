759dd85b472c5684f2a0598c6448066dc029ab0c
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-09-02 15:13:45 +0200

    Implement library matching

--- R/unformat.R
+++ R/unformat.R
@@ -38,6 +38,20 @@ unformat_session_info <- function(file) {
   )
   class(platform) <- c("platform_info", "list")
 
+  libs <- trimws(scan(
+    file,
+    what = character(),
+    sep = "\n",
+    skip = footnotes[2],
+    nlines = footnotes[3] - footnotes[2],
+    quiet = TRUE
+  ))
+
+  libs <- libs[startsWith(libs, "[")]
+  libs <- strsplit(libs, " ")
+  libs <- do.call(rbind, libs)
+  libs <- as.data.frame(libs, stringsAsFactors = TRUE)
+  colnames(libs) <- c("num", "path")
 
   packages_header <- x[packages_section + 1]
   colstarts <- regexec("\\s+(!?)\\s*(package)\\s+(\\*)\\s+(version)\\s+(date \\(UTC\\)|date)\\s+(lib)\\s+(source)", packages_header)[[1]]
@@ -53,12 +67,11 @@ unformat_session_info <- function(file) {
   packages$attached <- packages$attached == "*"
 
   # Guess
-  packages$library <- gsub("^\\[(\\d+)\\]\\s*", "\\1", packages$lib)
+  packages$library <- libs$path[match(packages$lib, libs$num)]
   packages$ondiskversion <- packages$loadedversion
   packages$md5ok <- NA
   packages$is_base <- FALSE
-  packages$path <- "placeholder"
-  packages$loadedpath <- "placeholder"
+  packages$path <- packages$loadedpath <- file.path(packages$library, packages$package)
 
   packages <- packages[, c("package", "ondiskversion", "loadedversion", "path", "loadedpath", "attached", "is_base", "date", "source", "md5ok", "library")]
   rownames(packages) <- packages$package
